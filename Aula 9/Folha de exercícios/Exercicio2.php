4e8413f586cf7076df6ab327dcf69ef59bc7973b
RafaZinke <xinxilarafael@gmail.com>
2024-10-01 17:18:48 -0300

    .

---
+++ Aula 9/Folha de exercícios/Exercicio2.php
@@ -0,0 +1,22 @@
+<?php
+/* Crie um programa para testar se um número é divisível por 2.
+Caso for verdade escrever a frase “Valor divisível por 2”
+Caso for falso escrever a frase “O valor não é divisível por 2” */ ?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>FolhaExercicios</title>
+    
+</head>
+<body>
+
+<?php
+
+teste
+?>
+
+</body>
+</html>
