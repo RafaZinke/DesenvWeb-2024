592e38809aa3818eb398ff538c20525031f320d7
RafaZinke <xinxilarafael@gmail.com>
2024-10-07 17:43:48 -0300

    .

--- Aula 9/Folha de exercícios/Ex7/funcoes.php
+++ Aula 9/Folha de exercícios/Ex7/funcoes.php
@@ -5,20 +5,3 @@ ficou em 60 parcelas de R$ 489,65.
 Escreva um programa que calcule o valor gasto só dos juros que serão pagos por
 Mariazinha em comparação ao valor a vista do carro. */ ?>
 
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>FolhaExercicios</title>
-    
-</head>
-<body>
-
-<?php
-
-
-?>
-
-</body>
-</html>
