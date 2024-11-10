6046a26049b1484c928a4b448729410354f3d930
RafaZinke <xinxilarafael@gmail.com>
2024-11-10 12:15:15 -0300

    .

---
+++ Aula 10/Pratica 2/index.php
@@ -0,0 +1,22 @@
+<?php
+// Inicia a sessão
+session_start();
+
+// Definindo um usuário e time diretamente no código
+$_SESSION['usuario'] = 'Rafael';
+$_SESSION['time'] = 'Botafogo';
+
+// Verifique se o usuário e o time foram definidos corretamente
+if (isset($_SESSION['usuario']) && isset($_SESSION['time'])) {
+    $usuario = $_SESSION['usuario'];
+    $time = $_SESSION['time'];
+    echo "Bem-vindo, $usuario! Você está no time: $time.";
+} else {
+    echo "Os dados da sessão foram perdidos";
+    exit;
+}
+
+session_destroy();
+
+
+?>
